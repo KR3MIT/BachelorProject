736d72bce0c4efc9b13ae38dbd02e0b8450e7c2f
William Abildtrup <william@abildtrup.eu>
2026-03-05 13:16:51 +0100

    shizzle

--- Assets/Scripts/UI/UIApprovalRating.cs
+++ Assets/Scripts/UI/UIApprovalRating.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,6 +13,7 @@ public class UIApprovalRating : MonoBehaviour
     {
         approvalRating = 0.5f;
         approvalBarFill.fillAmount = approvalRating;
+        approvalBarFade.enabled = false;
     }
 
 
@@ -20,10 +22,35 @@ public class UIApprovalRating : MonoBehaviour
         
     }
 
+    /* 
+     updatefill bar
+    removeapproval (set barfade to current barfill, barfill set to new approval rating, fade out barfade)
+    addapproval (set barfade current barfill + added approval rating, fill approvalbar over a lerp to barfade value)
+
+     
+     
+     */
+
     // fix this to update the bar fill based on the approval rating
     void UpdateBarFill()
     {
 
+    }
+
+    IEnumerator FadeBar()
+    {
+        float fadeDuration = 1f;
+        float elapsedTime = 0f;
+        Color initialColor = approvalBarFade.color;
+        Color targetColor = new Color(initialColor.r, initialColor.g, initialColor.b, 0f);
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / fadeDuration;
+            approvalBarFade.color = Color.Lerp(initialColor, targetColor, t);
+            yield return null;
+        }
+        approvalBarFade.color = targetColor;
     }
 
 }
