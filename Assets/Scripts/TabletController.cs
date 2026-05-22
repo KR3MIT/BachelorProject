da085e33820864d6b3acbeec0581e28778c7ccd1
KR3MIT <jason.cm.hassan@gmail.com>
2026-05-22 16:21:57 +0200

    final

--- Assets/Scripts/TabletController.cs
+++ Assets/Scripts/TabletController.cs
@@ -1,10 +1,14 @@
 using DG.Tweening;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class TabletController : MonoBehaviour
 {
     public static TabletController Instance { get; private set; }
+    public Volume ppVolume;
+    private DepthOfField dof;
 
     public Vector3 onScreenPosition;
     public Vector3 offScreenPosition;
@@ -12,25 +16,41 @@ public class TabletController : MonoBehaviour
     private void Awake()
     {
         Instance = this;
+
+        if (ppVolume != null)
+        {
+            ppVolume.profile.TryGet(out dof);
+        }
     }
 
     public async Task MoveOnScreen()
     {
+        SetDepthOfField(0.31f);
         await transform.DOMove(onScreenPosition, 1).SetEase(Ease.InOutSine).AsyncWaitForCompletion();
     }
 
     public void MoveOffScreen()
     {
+        SetDepthOfField(1.25f);
         transform.DOMove(offScreenPosition, 1).SetEase(Ease.InOutSine);
     }
 
     public async Task MoveOffScreen(bool hasTask)//whatever
     {
+        SetDepthOfField(1.25f);
         await transform.DOMove(offScreenPosition, 1).SetEase(Ease.InOutSine).AsyncWaitForCompletion();
     }
 
     public void TurnOnScreen()
     {
 
+    }
+
+    private void SetDepthOfField(float value)
+    {
+        if (dof != null)
+        {
+            dof.focusDistance.value = value;
+        }
     }
 }
